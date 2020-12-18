4f401fc3e4dd78da0557952827fb6489534ba05e
Liam Cooper <liamacooper@outlook.com>
2020-12-18 16:06:06 +0000

    add random day function

--- fortune_teller.js
+++ fortune_teller.js
@@ -1,7 +1,8 @@
+//chooses timeframe of week, month, year
 const timeframe = () => {
     let time = '';
     const randNum = Math.floor(Math.random() * 3);
-    console.log(randNum)
+    //console.log(randNum)
     switch (randNum) {
         case 0:
             return time = 'week'
@@ -17,5 +18,46 @@ const timeframe = () => {
     }
     return time;
 };
-
 console.log(timeframe());
+
+const dayMonth = timeframe();
+
+//chooses particular day or month
+const partDayMonth = () => {
+    let particular = '';
+        if (dayMonth === 'week') {
+            const randNumDay = Math.floor(Math.random() * 7)
+            switch (randNumDay) {
+                case 0:
+                    return particular = 'Monday';
+                    break;
+                case 1:
+                    return particular = 'Tuesday';
+                    break;
+                case 2:
+                    return particular = 'Wednesday';
+                    break;
+                case 3:
+                    return particular = 'Thursday';
+                    break;
+                case 4:
+                    return particular = 'Friday';
+                    break;
+                case 5:
+                    return particular = 'Saturday';
+                    break;
+                case 6:
+                    return particular = 'Sunday';
+                    break;
+                default:
+                    console.log('Issue choosing a day')
+                    break;
+            }
+        } else if (partDayMonth === 'month') {
+            const randNumMonth = Math.floor(Math.random() * 12)
+
+        }
+        return particular;
+};
+
+console.log(partDayMonth());
