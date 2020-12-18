8a7fa890bb60673aadc38b906bd2699a16c7ffe9
Liam Cooper <liamacooper@outlook.com>
2020-12-18 16:58:52 +0000

    swc which allows for a day/month to be selected

--- fortune_teller.js
+++ fortune_teller.js
@@ -1,63 +1,106 @@
 //chooses timeframe of week, month, year
-const timeframe = () => {
+function timeframe() {
     let time = '';
     const randNum = Math.floor(Math.random() * 3);
     //console.log(randNum)
     switch (randNum) {
         case 0:
-            return time = 'week'
+            time = 'week';
             break;
         case 1:
-            return time = 'month';
+            time = 'month';
             break;
         case 2:
-            return time = 'year';
+            time = 'year';
             break
         default:
             console.log('Issue with timeframe function')
     }
     return time;
 };
-console.log(timeframe());
 
-const dayMonth = timeframe();
+let timeFrame = timeframe();
+console.log(timeFrame);
 
 //chooses particular day or month
-const partDayMonth = () => {
-    let particular = '';
-        if (dayMonth === 'week') {
+function partDayMonth() {
+        if (timeFrame === 'week') {
             const randNumDay = Math.floor(Math.random() * 7)
+            let particular = '';
             switch (randNumDay) {
                 case 0:
-                    return particular = 'Monday';
+                    particular = 'Monday';
                     break;
                 case 1:
-                    return particular = 'Tuesday';
+                    particular = 'Tuesday';
                     break;
                 case 2:
-                    return particular = 'Wednesday';
+                    particular = 'Wednesday';
                     break;
                 case 3:
-                    return particular = 'Thursday';
+                    particular = 'Thursday';
                     break;
                 case 4:
-                    return particular = 'Friday';
+                    particular = 'Friday';
                     break;
                 case 5:
-                    return particular = 'Saturday';
+                    particular = 'Saturday';
                     break;
                 case 6:
-                    return particular = 'Sunday';
+                    particular = 'Sunday';
                     break;
                 default:
                     console.log('Issue choosing a day')
                     break;
-            }
-        } else if (partDayMonth === 'month') {
-            const randNumMonth = Math.floor(Math.random() * 12)
-
-        }
-        return particular;
+            };
+            return particular
+        };
+        if (timeFrame === 'month') {
+            const randNumMonth = Math.floor(Math.random() * 12);
+            let particular = '';
+            switch (randNumMonth) {
+                case 0:
+                    particular = 'January';
+                    break;
+                case 1:
+                    particular = 'February';
+                    break;
+                case 2:
+                    particular = 'March';
+                    break;
+                case 3:
+                    particular = 'April';
+                    break;
+                case 4:
+                    particular = 'May';
+                    break;
+                case 5:
+                    particular = 'June';
+                    break;
+                case 6:
+                    particular = 'July';
+                    break;
+                case 7:
+                    particular = 'August';
+                    break;
+                case 8:
+                    particular = 'September';
+                    break;
+                case 9:
+                    particular = 'October';
+                    break;
+                case 10:
+                    particular = 'November';
+                    break;
+                case 11:
+                    particular = 'December';
+                    break;
+                default:
+                    console.log('Unable to select a particular Month')
+                    break;
+            };
+            return particular
+        };
 };
-
+//console.log(timeFrame);
 console.log(partDayMonth());
